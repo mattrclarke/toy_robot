9e90c07d371e031ee69f652b425636e89416f36f
Matt Clarke <matt.clarke85@gmail.com>
2020-06-08 20:02:24 +1000

    update generate world method

--- models/world.rb
+++ models/world.rb
@@ -3,16 +3,16 @@ class World
   def initialize(width, height)
     @width = width 
     @height = height
-    generate_world
+    generate_world(@width, @height)
   end
   
   def is_valid_position? (y, x)
-    y.between?(0, @width) && x.between?(0, @height)
+    y.between?(0, @width - 1) && x.between?(0, @height - 1)
   end
   
   private
-  def generate_world
-    [*0..@height].product([*0..@width]).each_slice(@width + 1).to_a
+  def generate_world(height, width)
+    [*0..height - 1].product([*0..width - 1]).each_slice(width).to_a
   end
   
 end
