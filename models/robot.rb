fcc15a0371fa77f929e6975373c0998310cfd1f8
Matt Clarke <matt.clarke85@gmail.com>
2020-06-08 19:48:37 +1000

    remove testing code

--- models/robot.rb
+++ models/robot.rb
@@ -35,9 +35,9 @@ class Robot
   private
   def place_robot
     p "enter an x coordinate to place the robot"
-    @x_coords = gets.chomp.to_i || 0
+    @x_coords = gets.chomp.to_i
     p "enter a y coordinate to place the robot"
-    @y_coords = gets.chomp.to_i || 0
+    @y_coords = gets.chomp.to_i
         
     @movement_controller = MovementController.new(@y_coords, @x_coords, 0, @world)
     @world.is_valid_position?(@y_coords, @x_coords)
